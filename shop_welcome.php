fd9fe984d11d8f26a5ca2b1a9c5c22a8b5e4393b
Jonathan Rosenbaum <gnuser@gmail.com>
2015-01-14 07:04:11 +0000

    More to the attic, PSised welcome page. :)

--- shop_welcome.php
+++ shop_welcome.php
@@ -5,25 +5,32 @@ require_once('Connections/database_functions.php');
 ?>
 
 <?php include("include_header.html"); ?>
-      <p><span class="yb_heading2">Welcome to the Austin Yellow Bike Project</span></p>
+      <p><span class="yb_heading2">Welcome to the Positive Spin Bike Project</span></p>
       <p>Here are a few things to know about using the shop:</p>
       <ul>
-        <li><span class="yb_heading3red">This is Your Community Bike Shop; it is free of charge and open to the public</span>, providing a space for people to work on bikes, and learn bike mechanics skills.</li>
+        <li><span class="yb_heading3red">This is Your Community Bike Shop; it is free of charge and open to the public</span>, 
+        providing a space for people to work on bikes, and learn bike mechanics skills.</li>
       </ul>
       <ul>
-        <li>Yellow Bike is an all-volunteer non-profit organization  <span class="yb_heading3red">entirely supported by volunteer time, part donations, and money  donations</span>. </li>
+        <li>Positive Spin is an all-volunteer non-profit organization  
+        <span class="yb_heading3red">entirely supported by volunteer time, part donations, and money  donations</span>. </li>
       </ul>
       <ul>
-        <li><span class="yb_heading3red">We expect that you volunteer time back  to the project</span> equal to the time in the shop spent on personal projects to leave the project a better place than you found it. </li>
+        <li><span class="yb_heading3red">We expect that you volunteer time back  to the project</span> 
+        equal to the time in the shop spent on personal projects to leave the project a better place than you found it. </li>
       </ul>
       <ul>
-        <li>If you are unable to contribute time to the project <span class="yb_heading3red">we suggest  a $5 donation for personal use of the shop</span> in addition to any donations made for  parts. </li>
+        <li>If you are unable to contribute time to the project <span class="yb_heading3red">
+        we suggest  a $5 donation for personal use of the shop</span> in addition to any donations made for  parts. </li>
       </ul>
       <ul>
-        <li><span class="yb_heading3red">Donations go towards</span> shop tools and supplies as well as  helping YBP meet our other programming needs.</li>
+        <li><span class="yb_heading3red">Donations go towards</span> 
+        shop tools and supplies as well as  helping PS meet our other programming needs.</li>
       </ul>
       <ul>
-        <li><span class="yb_heading3red">To get started,</span> just sign-in and  talk to one of the coordinators. <span class="yb_heading3red">Make sure to sign-out</span> when you are done. </li>
+        <li><span class="yb_heading3red">To get started,</span> 
+        just sign-in and  talk to one of the coordinators. <span class="yb_heading3red">Make sure to sign-out</span> 
+        when you are done. </li>
       </ul>
       <table height="40" border="1" align="center" cellpadding="1" cellspacing="0">
       <tr align="center">
@@ -33,13 +40,19 @@ require_once('Connections/database_functions.php');
           </span><span class="yb_standardCENTERred">Talk to a coordinator</span></td>
         <td width="203"><span class="style8"><span class="style9"><a href="shop_log.php">Sign Out</a> Before Leaving</span><br /> 
           </span><span class="yb_standardCENTERred">Workspace cleaned up?</span></td>
-        <td width="155"><span class="style8"><span class="style9"><a href="survey.php"> Take Our Survey!</a></span><br />
-        </span><span class="yb_standardCENTERred">How are  we doing?</span></td>
       </tr>
     </table>
     <p><br />
       <span class="yb_pagetitle">Learn More</span>:<br />
-        <span class="yb_heading3red">YBP Info:   </span><a href="http://www.austinyellowbike.org/yb_about_ybp.htm" target="_blank">Yellow Bike Home Page</a> | <a href="http://www.austinyellowbike.org/yb_about_ybp.htm" target="_blank">About YBP</a> | <a href="http://www.austinyellowbike.org/yb_calendar.html" target="_blank">Shop Schedule </a> | <a href="http://www.austinyellowbike.org/yb_newsletters.htm" target="_blank">Monthly Newsletter</a> | <a href="http://www.austinyellowbike.org/yb_services.htm" target="_blank">Shop Services</a><span class="yb_heading3red"><br />
-        Giving Back:</span> <a href="http://www.austinyellowbike.org/yb_participate.htm" target="_blank">Volunteering at YBP</a> | <a href="http://www.austinyellowbike.org/yb_services.htm" target="_blank">Volunteer Shops</a> | <a href="http://www.austinyellowbike.org/yb_projects.htm" target="_blank">Projects</a> | <a href="http://www.austinyellowbike.org/yb_bikes.htm" target="_blank">Earn-A-Bike</a> | <a href="http://www.austinyellowbike.org/yb_donate.htm" target="_blank">Donating Online</a><br />
+        <span class="yb_heading3red">PS Info:   </span>
+        <a href="http://positivespin.org" target="_blank">Positive Spin Home Page</a> | 
+        <a href="http://positivespin.org/home2/content/view/34/71/" target="_blank">About PS</a> | 
+        <a href="http://positivespin.org/home2/content/view/94/80/" target="_blank">Shop Schedule </a> | 
+        <a href="http://positivespin.org/home2/content/view/13/14/" target="_blank">Shop Services</a><span class="yb_heading3red"><br />
+        Giving Back:</span> <a href="" target="_blank">Volunteering at YBP</a> | 
+        <a href="http://positivespin.org/home2/content/view/94/80/" target="_blank">Volunteer Shops</a> | 
+        <a href="http://positivespin.org/home2/content/view/92/79/" target="_blank">Projects</a> | 
+        <a href="" target="_blank">Earn-A-Bike</a> | 
+        <a href="" target="_blank">Donating Online</a><br />
     </p>
     <?php include("include_footer.html"); ?>
