2d12b59786b47bc7a4a811270cf29716a07edcda
Jonathan Rosenbaum <gnuser@gmail.com>
2017-07-15 18:14:44 +0000

    You may now use Connections/local_configurations.php for configurations.

--- Connections/YBDB.php
+++ Connections/YBDB.php
@@ -1,4 +1,6 @@
 <?php
+// DO NOT EDIT - USE Connections/local_configurations instead
+
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
@@ -6,5 +8,9 @@ $hostname_YBDB = "enter_hostname";
 $database_YBDB = "enter_schema";
 $username_YBDB = "enter_username";
 $password_YBDB = "enter_password";
-$YBDB = mysql_pconnect($hostname_YBDB, $username_YBDB, $password_YBDB) or trigger_error(mysql_error(),E_USER_ERROR); 
+if(file_exists('Connections/local_configurations.php')) {
+	require_once('local_configurations.php');
+}
+$YBDB = mysql_pconnect($hostname_YBDB, $username_YBDB, $password_YBDB) or trigger_error(mysql_error(),E_USER_ERROR);
+
 ?>
