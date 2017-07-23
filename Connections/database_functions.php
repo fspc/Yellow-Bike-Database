7969da316c1c8cbd7915395c1a3c722be3284e8b
Jonathan Rosenbaum <gnuser@gmail.com>
2017-07-23 17:13:48 +0000

    Adds a missing ; in new definition.

--- Connections/database_functions.php
+++ Connections/database_functions.php
@@ -47,7 +47,7 @@ STAND TIME
 define("STAND_TIME_HOURLY_RATE",10);
 
 // Define how much time over 1hr is allowed before charging for the next hour.
-define("STAND_TIME_GRACE_PERIOD",15)
+define("STAND_TIME_GRACE_PERIOD",15);
 
 /********************************
 SWEAT EQUITY / VOLUNTEER BENEFITS
