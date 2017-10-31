f742fc9621694c047397803999352862f3f1f85e
Jonathan Rosenbaum <gnuser@gmail.com>
2017-10-31 05:20:47 +0000

    Adds prev next buttons as req in enhancement #12

--- individual_history_log.php
+++ individual_history_log.php
@@ -20,7 +20,12 @@ include("include_header.html");
 ?>
 
 <div id="contact_id" style="display:none;"><?php echo $contact_id; ?></div>
-<center><div id="name" style="align-content:center; font-size:2em;"></div></center>
+<div style="text-align:center;"><div id="name" style="align-content:center; font-size:2em;"></div></div>
 <div id="individual_history"></div>
+
+<div class="stats-clear nextprev" style="text-align:center; display:block; margin-top:9px;">
+<a class="stats-left stats-btn" href="#">&#10094; Previous</a>
+<a class="stats-right stats-btn" href="#">Next &#10095;</a>
+</div>
 
 <?php include("include_footer.html"); ?>
