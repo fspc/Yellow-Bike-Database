fspc/Yellow-Bike-Database

commit 34650c9be0995bbf9193aa4a1617b065e38e99d6

Mozilla, not chrome, picked up on the strict javascript requirement.
* some javascript libraries failed, but not all
* put volunteer_hours_to_membership_discount() in the right place, right above redeemable().
* Fixed!

js/transaction.js
@@ -607,6 +607,23 @@ $(function() {
 			
 	} // end function save_or_close
 	
+	// invoke when volunteer hours run out for a member with a qualifying transaction
+	function volunteer_hours_to_membership_discount(price) {
+				
+		var discount = (price * (membership_obj.membership_discount / 100).toFixed(2)).toFixed(2);
+		var discount_price = (price - discount).toFixed(2);		
+		if ( $("#transaction_type").val() !== "Stand Time" ) {
+			$("#membership_discount").text("Member pays $" + discount_price).show();
+			$("#membership_discount_price").text(discount_price);
+			$("#redeemable_hours").spinner("disable");
+			//amount.prop("disabled","disabled");
+		} else {
+			$("#membership_discount").empty();
+			$("#membership_discount_price").empty();
+		}					
+									
+	} // end function volunteer_hours_to_membership_discount	
+	
 	// volunteer hours magic
 	function redeemable(obj, spinner_value, event, volunteer) {
 		
@@ -1234,23 +1251,7 @@ $(function() {
 					}										
 									
 				}); // volunteers post
-
-				// invoke when volunteer hours run out for a member with a qualifying transaction
-				function volunteer_hours_to_membership_discount(price) {
-							
-					var discount = (price * (membership_obj.membership_discount / 100).toFixed(2)).toFixed(2);
-					var discount_price = (price - discount).toFixed(2);		
-					if ( $("#transaction_type").val() !== "Stand Time" ) {
-						$("#membership_discount").text("Member pays $" + discount_price).show();
-						$("#membership_discount_price").text(discount_price);
-						$("#redeemable_hours").spinner("disable");
-						//amount.prop("disabled","disabled");
-					} else {
-						$("#membership_discount").empty();
-						$("#membership_discount_price").empty();
-					}					
-												
-				} // end function volunteer_hours_to_membership_discount
+				
 				
 				// Free stand time use for 30 days if purchased bike recently
 				if ($("#transaction_type").val() === "Stand Time") {

commit e99d2863b85b8d3b65df567f2722f62090b85ba7

This fixes a bug introduced by the 34650c9be fix.
* There is an issue if the spinner is used, when the transaction is complete, the function is called infinitely.

js/transaction.js
@@ -608,7 +608,7 @@ $(function() {
 	} // end function save_or_close
 	
 	// invoke when volunteer hours run out for a member with a qualifying transaction
-	function volunteer_hours_to_membership_discount(price) {
+	function volunteer_hours_to_membership_discount(price, membership_obj) {
 				
 		var discount = (price * (membership_obj.membership_discount / 100).toFixed(2)).toFixed(2);
 		var discount_price = (price - discount).toFixed(2);		
@@ -880,7 +880,7 @@ $(function() {
 				// Is this a paid member?	
 				// Determine membership benefits of current transaction
 				$.post("json/transaction.php", { membership_benefits: 1, contact_id: this.value }, function (data) { 
-												
+																
 					membership_obj = $.parseJSON(data);
 					var title = membership_obj.normal_full_name + "\r\n" +
 											membership_obj.email + "\r\n" +
@@ -1148,14 +1148,14 @@ $(function() {
 										redeemable(obj, ui.value, event, volunteer);
 									} else {
 										$(this).spinner("disable");
-									}
+									}									
 									
 									// good place for a function to handle bug #4
 							   	if (obj.volunteer && current_membership === true) {
 								   	if (remaining === ui.value && remaining !== 0 && ui.value !== 0) {
 											if ( typeof amount.val() !== 'undefined') {
 												var price = amount.cleanVal();			
-												volunteer_hours_to_membership_discount(price);
+												volunteer_hours_to_membership_discount(price, membership_obj);
 											}
 										}	
 									}						   	
@@ -1201,7 +1201,7 @@ $(function() {
 							   	if (remaining === Number(val) && remaining !== 0 && Number(val) !== 0) {
 										if ( typeof amount.val() !== 'undefined') {	
 											var price = amount.cleanVal();
-											volunteer_hours_to_membership_discount(price);
+											volunteer_hours_to_membership_discount(price, membership_obj);
 										}
 									}								   
 								}						   